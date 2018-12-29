150c873dcdf3d940641670d77ff19ecb9b66078c
dceoy <dnarsil+github@gmail.com>
2018-12-29 17:06:51 +0900

    Mangle attributes

--- oandacli/call/streamer.py
+++ oandacli/call/streamer.py
@@ -19,84 +19,84 @@ class StreamDriver(oandapy.Streamer):
                  redis_host='127.0.0.1', redis_port=6379, redis_db=0,
                  redis_max_llen=None, sqlite_path=None, csv_path=None,
                  quiet=False):
-        self.logger = logging.getLogger(__name__)
+        self.__logger = logging.getLogger(__name__)
         super().__init__(
             environment=config_dict['oanda']['environment'],
             access_token=config_dict['oanda']['access_token']
         )
-        self.account_id = config_dict['oanda']['account_id'],
+        self.__account_id = config_dict['oanda']['account_id'],
         if target in ['rate', 'event']:
-            self.key = {'rate': 'tick', 'event': 'transaction'}[target]
+            self.__data_key = {'rate': 'tick', 'event': 'transaction'}[target]
         else:
             raise OandaCliRuntimeError(
                 'invalid stream target: {}'.format(target)
             )
-        self.instruments = (
+        self.__instruments = (
             instruments if instruments else config_dict['instruments']
         )
-        self.ignore_heartbeat = ignore_heartbeat
-        self.print_json = print_json
-        self.quiet = quiet
+        self.__ignore_heartbeat = ignore_heartbeat
+        self.__print_json = print_json
+        self.__quiet = quiet
         if use_redis:
-            self.logger.info('Set a streamer with Redis')
-            self.redis_pool = redis.ConnectionPool(
+            self.__logger.info('Set a streamer with Redis')
+            self.__redis_pool = redis.ConnectionPool(
                 host=redis_host, port=int(redis_port), db=int(redis_db)
             )
-            self.redis_max_llen = (
+            self.__redis_max_llen = (
                 int(redis_max_llen) if redis_max_llen else None
             )
-            redis_c = redis.StrictRedis(connection_pool=self.redis_pool)
+            redis_c = redis.StrictRedis(connection_pool=self.__redis_pool)
             redis_c.flushdb()
         else:
-            self.redis_pool = None
-            self.redis_max_llen = None
+            self.__redis_pool = None
+            self.__redis_max_llen = None
         if sqlite_path:
-            self.logger.info('Set a streamer with SQLite')
+            self.__logger.info('Set a streamer with SQLite')
             sqlite_abspath = os.path.abspath(
                 os.path.expanduser(os.path.expandvars(sqlite_path))
             )
             if os.path.isfile(sqlite_abspath):
-                self.sqlite = sqlite3.connect(sqlite_abspath)
+                self.__sqlite = sqlite3.connect(sqlite_abspath)
             else:
                 schema_sql_path = os.path.join(
                     os.path.dirname(__file__), '../static/create_tables.sql'
                 )
                 with open(schema_sql_path, 'r') as f:
                     sql = f.read()
-                self.sqlite = sqlite3.connect(sqlite_abspath)
-                self.sqlite.executescript(sql)
+                self.__sqlite = sqlite3.connect(sqlite_abspath)
+                self.__sqlite.executescript(sql)
         else:
-            self.sqlite = None
+            self.__sqlite = None
         if csv_path:
-            self.csv_path = os.path.abspath(
+            self.__csv_path = os.path.abspath(
                 os.path.expanduser(os.path.expandvars(csv_path))
             )
         else:
-            self.csv_path = None
+            self.__csv_path = None
 
     def on_success(self, data):
         data_json_str = json.dumps(data)
-        if self.quiet:
-            self.logger.debug(data)
-        elif self.print_json:
+        if self.__quiet:
+            self.__logger.debug(data)
+        elif self.__print_json:
             print(data_json_str, flush=True)
         else:
             print(yaml.dump(data).strip(), flush=True)
         if 'disconnect' in data:
-            self.logger.warning('Streaming disconnected: {}'.format(data))
+            self.__logger.warning('Streaming disconnected: {}'.format(data))
             self.shutdown()
-        elif self.key in data:
-            self.logger.debug(data)
-            if self.redis_pool:
-                instrument = data[self.key]['instrument']
-                redis_c = redis.StrictRedis(connection_pool=self.redis_pool)
+        elif self.__data_key in data:
+            self.__logger.debug(data)
+            if self.__redis_pool:
+                instrument = data[self.__data_key]['instrument']
+                redis_c = redis.StrictRedis(connection_pool=self.__redis_pool)
                 redis_c.rpush(instrument, data_json_str)
-                if self.redis_max_llen:
-                    if redis_c.llen(instrument) > self.redis_max_llen:
+                if self.__redis_max_llen:
+                    if redis_c.llen(instrument) > self.__redis_max_llen:
                         redis_c.lpop(instrument)
-            if self.sqlite:
-                c = self.sqlite.cursor()
-                if self.key == 'tick':
+            if self.__sqlite:
+                c = self.__sqlite.cursor()
+                if self.__data_key == 'tick':
                     c.execute(
                         'INSERT INTO tick VALUES (?,?,?,?)',
                         [
@@ -104,8 +104,8 @@ class StreamDriver(oandapy.Streamer):
                             data['tick']['bid'], data['tick']['ask']
                         ]
                     )
-                    self.sqlite.commit()
-                elif self.key == 'transaction':
+                    self.__sqlite.commit()
+                elif self.__data_key == 'transaction':
                     c.execute(
                         'INSERT INTO transaction VALUES (?,?,?)',
                         [
@@ -114,44 +114,44 @@ class StreamDriver(oandapy.Streamer):
                             json.dumps(data['transaction'])
                         ]
                     )
-                    self.sqlite.commit()
-            if self.csv_path:
-                pd.DataFrame([data[self.key]]).set_index(
+                    self.__sqlite.commit()
+            if self.__csv_path:
+                pd.DataFrame([data[self.__data_key]]).set_index(
                     'time', drop=True
                 ).to_csv(
-                    self.csv_path, mode='a',
-                    sep=(',' if self.csv_path.endswith('.csv') else '\t'),
-                    header=(not os.path.isfile(self.csv_path))
+                    self.__csv_path, mode='a',
+                    sep=(',' if self.__csv_path.endswith('.csv') else '\t'),
+                    header=(not os.path.isfile(self.__csv_path))
                 )
         else:
-            self.logger.warning('Save skipped: {}'.format(data))
+            self.__logger.warning('Save skipped: {}'.format(data))
 
     def on_error(self, data):
-        self.logger.error(data)
+        self.__logger.error(data)
         self.shutdown()
 
     def invoke(self):
         signal.signal(signal.SIGINT, signal.SIG_DFL)
-        if self.key == 'tick':
-            self.logger.info('Start to stream market prices')
+        if self.__data_key == 'tick':
+            self.__logger.info('Start to stream market prices')
             self.rates(
-                account_id=self.account_id,
-                ignore_heartbeat=self.ignore_heartbeat,
-                instruments=','.join(self.instruments)
+                account_id=self.__account_id,
+                ignore_heartbeat=self.__ignore_heartbeat,
+                instruments=','.join(self.__instruments)
             )
-        elif self.key == 'transaction':
-            self.logger.info('Start to stream events for the account')
+        elif self.__data_key == 'transaction':
+            self.__logger.info('Start to stream events for the account')
             self.events(
-                account_id=self.account_id,
-                ignore_heartbeat=self.ignore_heartbeat
+                account_id=self.__account_id,
+                ignore_heartbeat=self.__ignore_heartbeat
             )
 
     def shutdown(self):
         self.disconnect()
-        if self.redis_pool:
-            self.redis_pool.disconnect()
-        if self.sqlite:
-            self.sqlite.close()
+        if self.__redis_pool:
+            self.__redis_pool.disconnect()
+        if self.__sqlite:
+            self.__sqlite.close()
 
 
 def invoke_streamer(config_yml, target='rate', instruments=None, csv_path=None,
