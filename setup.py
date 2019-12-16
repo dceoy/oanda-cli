1e688bd11e07f53fbb79e4f97db267b82fe04920
dceoy <dnarsil+github@gmail.com>
2019-12-17 03:59:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,28 +10,28 @@ with open('README.md', 'r') as f:
 setup(
     name='oanda-cli',
     version=__version__,
-    description='Command Line Interface for Oanda API',
-    packages=find_packages(),
     author='Daichi Narushima',
     author_email='dnarsil+github@gmail.com',
+    description='Command Line Interface for Oanda API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/dceoy/oanda-cli',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'docopt', 'pandas', 'pyyaml', 'redis', 'ujson', 'v20'
     ],
-    entry_points={
-        'console_scripts': ['oanda-cli=oandacli.cli.main:main'],
-    },
+    entry_points={'console_scripts': ['oanda-cli=oandacli.cli.main:main']},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
         'Programming Language :: Python :: 3',
         'Topic :: Office/Business :: Financial :: Investment'
     ],
-    python_requires='>=3.6',
-    long_description=long_description
+    python_requires='>=3.6'
 )
